f81396ccc91475b4e9b0dc54e2aa0e8347eaa802
yvan <ajdnui@163.com>
2020-05-03 12:06:16 +0800

    Default Changelist

--- src/main/java/com/zy/hellospring/controller/First.java
+++ src/main/java/com/zy/hellospring/controller/First.java
@@ -41,33 +41,7 @@ public class First {
         cc.setCc2("c2222");
         apiRes.setCc(cc);
 
-        EE ee = new EE();
-        ee.setUnitId(0);
-        ee.setProductId(0);
-        ee.setAddPeopleUnitId(0);
-        ee.setAddPeopleProductId(0);
-        ee.setCleanUnitId(0);
-        ee.setCleanProductId(0);
-        ee.setALLUnitId(0);
-        ee.setALLProductId(0);
-        ee.setHotelId(0);
-        ee.setLandlordTel("");
-        ee.setUserId(0);
-        ee.setUserTel(0L);
-        ee.setApikey("");
-        HouseimportBean houseimportBean = new HouseimportBean();
-        houseimportBean.setComment("");
-        houseimportBean.setPartner_hotel_id("");
-        houseimportBean.setHotelId(0);
-        houseimportBean.setPartner_unit_id("");
-        houseimportBean.setUnitId(0);
-        houseimportBean.setPartner_product_id("");
-        houseimportBean.setProductId(0);
-
-
-
-        ee.setHouseimport(houseimportBean);
-
+        
 
 
         return apiRes;
